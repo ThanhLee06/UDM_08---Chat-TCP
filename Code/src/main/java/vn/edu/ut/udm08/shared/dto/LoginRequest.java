0b196461aa1a0a5894adcda27704c4a5577fb696
ThanhLee06 <lexuanthanh0012@gmail.com>
2026-09-02 14:43:27 +0700

    feat(ST-066): thêm DTO đăng nhập

---
+++ Code/src/main/java/vn/edu/ut/udm08/shared/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package vn.edu.ut.udm08.shared.dto;
+public class LoginRequest {
+    private String phoneNumber;
+    private String password;
+    public LoginRequest() {}
+    public LoginRequest(String phoneNumber, String password) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ Code/src/main/java/vn/edu/ut/udm08/shared/dto/LoginResponse.java
@@ -0,0 +1,37 @@
+package vn.edu.ut.udm08.shared.dto;
+import vn.edu.ut.udm08.shared.model.User;
+public class LoginResponse {
+    private boolean success;
+    private String message;
+    private User user;
+    public LoginResponse() {}
+    public LoginResponse(boolean success, String message, User user) {
+        this.success = success;
+        this.message = message;
+        this.user = user;
+    }
+    public static LoginResponse ok(String message, User user) {
+        return new LoginResponse(true, message, user);
+    }
+    public static LoginResponse fail(String message) {
+        return new LoginResponse(false, message, null);
+    }
+    public boolean isSuccess() {
+        return success;
+    }
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
