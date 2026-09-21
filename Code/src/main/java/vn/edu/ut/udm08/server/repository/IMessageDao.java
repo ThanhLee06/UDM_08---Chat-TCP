07bab30bd63e8ff440bbe01e00e0f91cf6be7ff8
huutienSE <huutiense25@gmail.com>
2026-09-22 00:53:35 +0700

    feat(ST-101): define message dao contract

---
+++ Code/src/main/java/vn/edu/ut/udm08/server/repository/IMessageDao.java
@@ -0,0 +1,11 @@
+package vn.edu.ut.udm08.server.repository;
+import vn.edu.ut.udm08.server.model.ChatMessage;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+public interface IMessageDao {
+    ChatMessage insertMessage(ChatMessage message);
+    ChatMessage insertMessage(Connection conn, ChatMessage message);
+    Optional<ChatMessage> findByMessageId(String messageId);
+    List<ChatMessage> findByConvId(String convId, Long beforeSequenceId, int limit);
+}
