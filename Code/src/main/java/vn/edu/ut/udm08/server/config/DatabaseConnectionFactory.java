8be5a750344ccfbdffaa05e9a8cf647a21b6762d
huutienSE <huutiense25@gmail.com>
2026-09-22 19:02:19 +0700

    feat(ST-104): configure database connection and initialization

--- Code/src/main/java/vn/edu/ut/udm08/server/config/DatabaseConnectionFactory.java
+++ Code/src/main/java/vn/edu/ut/udm08/server/config/DatabaseConnectionFactory.java
@@ -4,22 +4,38 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 public class DatabaseConnectionFactory {
+    
     private final String dbUrl;
+    private final int busyTimeout;
+
     public DatabaseConnectionFactory() {
-        this(System.getProperty("udm08.db.url", "jdbc:sqlite:udm08_chat.db"));
+        this(System.getProperty("udm08.db.url", "jdbc:sqlite:udm08_chat.db"), 5000);
     }
+
     public DatabaseConnectionFactory(String dbUrl) {
-        this.dbUrl = dbUrl;
+        this(dbUrl, 5000);
     }
+
+    public DatabaseConnectionFactory(String dbUrl, int busyTimeout) {
+        this.dbUrl = dbUrl;
+        this.busyTimeout = busyTimeout > 0 ? busyTimeout : 5000;
+    }
+
+
     public Connection getConnection() throws SQLException {
         Connection conn = DriverManager.getConnection(dbUrl);
         try (Statement stmt = conn.createStatement()) {
             stmt.execute("PRAGMA foreign_keys = ON;");
-            stmt.execute("PRAGMA busy_timeout = 5000;");
+            stmt.execute("PRAGMA busy_timeout = " + busyTimeout + ";");
         }
         return conn;
     }
+
     public String getDbUrl() {
         return dbUrl;
+    }
+
+    public int getBusyTimeout() {
+        return busyTimeout;
     }
 }
